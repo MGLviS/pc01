00c9ea599ceae9ebb92c38937974a9c621672c8c
LuisM <118645322+MGLviS@users.noreply.github.com>
2023-09-09 20:44:32 -0500

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function redAL() {
+  location.href = "https://clubalianzalima.com.pe/";
+}
+
+function redGRA() {
+  location.href = "https://twitter.com/Grau_Oficial";
+}
+
+function redCUS() {
+  location.href = "https://twitter.com/Cusco_F_C";
+}
+
+function redSBA() {
+  location.href = "https://twitter.com/sportboys";
+}
